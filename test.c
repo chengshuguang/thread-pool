45a1b9649d09ec493a8404bcffee9bc390a02713
chengshuguang <594298131@qq.com>
2013-12-05 10:15:48 +0800

    a stable version

--- test.c
+++ test.c
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
 #include "thread_pool.h"
-#include <stdio.h>
-
-void *taskprocess(void *arg)
-{
-	printf("aaaaaadoing tasksaaaaaaaaa\n");
-	usleep(1000);
-	return NULL;
-}
-
-
-int main()
-{
-	thread_pool_init(5);
-	int i;
-	for(i=1; i<=10; i++)
-	{
-		thread_pool_add_task(taskprocess,(void *)i);
-		usleep(1000);
-	}
-	sleep(1);
-	thread_pool_destroy();
-	return 0;
-}
-=======
-#include <thread_pool.h>
 #include <stdio.h>
 
 void *taskprocess(void *arg)
@@ -48,4 +22,3 @@ int main()
 	thread_pool_destroy();
 	return 0;
 }
->>>>>>> ccc0f1fff880b35c72d9a61794d7a35b989f8898
